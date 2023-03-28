ad5627568acbd8505dea44bef65e0109ef0ee5ce
swapfr <97851724+swapfr@users.noreply.github.com>
2023-03-28 22:07:52 +0300

    Update script.py

--- script.py
+++ script.py
@@ -4,26 +4,22 @@ import struct
 from bpy_extras.io_utils import ( 
     ImportHelper 
 ) 
-####   !!!Set True if you want the object to be copied instead of instancing (True) (False)
-####    ___________________________________________________________________
-
-instancing_check = False
-
-####
-####
-
   
 print("====" * 30)
+
 class CustomDrawOperator(bpy.types.Operator, ImportHelper): 
     bl_idname = "object.custom_draw" 
     bl_label = "Import" 
   
     filepath = bpy.props.StringProperty(subtype="FILE_PATH") 
-  
+    
+    instancing_check1 : bpy.props.BoolProperty(name = 'Import as instancing', description = 'Description', default = False)
+
     def execute(self, context): 
+        instancing_check = not (self.instancing_check1)
         print(self.filepath) 
         print("\n\n")
- 
+
         encoding = "cp1251" 
         ipl_path = self.filepath 
  
@@ -66,15 +62,17 @@ class CustomDrawOperator(bpy.types.Operator, ImportHelper):
                     new_obj.rotation_quaternion = (-(coords_rot[6]), coords_rot[3], coords_rot[4], coords_rot[5])
                     new_obj.name = f"{obj_name}_{i}"
                     bpy.data.collections[collection_name].objects.link(new_obj)
-                    print(f"{new_obj.name} location: {new_obj.location} rotation: (x = {coords_rot[3]}, y = {coords_rot[4]}, z = {coords_rot[5]}, w = {coords_rot[6]})")
-                    print("\n")
+                    
+                    #print(f"{new_obj.name} location: {new_obj.location} rotation: (x = {coords_rot[3]}, y = {coords_rot[4]}, z = {coords_rot[5]}, w = {coords_rot[6]})")
+                    #print("\n")
             elif check == 1:
                 coords_rot = coords_rot_list[0]
                 obj.location = coords_rot[0:3] 
                 obj.rotation_mode = 'QUATERNION'
                 obj.rotation_quaternion = (-(coords_rot[6]), coords_rot[3], coords_rot[4], coords_rot[5])
-                print(f"{obj_name} location: {obj.location} rotation: (x = {coords_rot[3]}, y = {coords_rot[4]}, z = {coords_rot[5]}, w = {coords_rot[6]})")
-                print("\n")
+                
+                #print(f"{obj_name} location: {obj.location} rotation: (x = {coords_rot[3]}, y = {coords_rot[4]}, z = {coords_rot[5]}, w = {coords_rot[6]})")
+                #print("\n")
             else:
                 
                 collection_name = obj_name + "_collection_inst"
@@ -85,7 +83,6 @@ class CustomDrawOperator(bpy.types.Operator, ImportHelper):
                     
                     bpy.context.scene.collection.children.link(new_collection)
                 for i, coords_rot in enumerate(coords_rot_list):
-                    
                     bpy.ops.object.empty_add(type='PLAIN_AXES', align='WORLD', location=(0.0, 0.0, 0.0), scale=(1, 1, 1))                   
                     obj_inst = bpy.context.object
                     obj_inst.name = f"{obj_name}_{i}"      
@@ -95,9 +92,11 @@ class CustomDrawOperator(bpy.types.Operator, ImportHelper):
                     bpy.data.collections[collection_name].objects.link(obj_inst)
                     obj_inst.instance_type = 'COLLECTION'
                     obj_inst.instance_collection = bpy.data.collections[obj_name + ".dff"]
-                    print(f"{obj_inst.name} location: {obj_inst.location} rotation: (x = {coords_rot[3]}, y = {coords_rot[4]}, z = {coords_rot[5]}, w = {coords_rot[6]})")
-                    print("\n")
-
+                    
+                    #print(f"{obj_inst.name} location: {obj_inst.location} rotation: (x = {coords_rot[3]}, y = {coords_rot[4]}, z = {coords_rot[5]}, w = {coords_rot[6]})")
+                    #print("\n")
+        
+        print ("Done")
 
         return {'FINISHED'} 
  
