cd99b3ce4b3d8780c4f5efe62d3ea459a996711e
swapfr <97851724+swapfr@users.noreply.github.com>
2023-03-29 18:20:59 +0300

    Update ipl_format_line_in_console.py

--- ipl_format_line_in_console.py
+++ ipl_format_line_in_console.py
@@ -1,5 +1,6 @@
 import bpy 
 import struct 
+ 
 
 selected_objects = bpy.context.selected_objects
 
@@ -12,10 +13,10 @@ for obj in selected_objects:
     
     if "." in obj.name:
         string= obj.name.split(".")[0]
-        print(f"  0000, {string}, 0, {coord.x:.3f}, {coord.y:.3f}, {coord.z:.3f}, {rot.x:.3f}, {rot.y:.3f}, {rot.z:.3f}, {-(rot.w):.3f}, -1")
-
-    else:
         
-        print(f"  0000, {obj.name}, 0, {coord.x:.3f}, {coord.y:.3f}, {coord.z:.3f}, {rot.x:.3f}, {rot.y:.3f}, {rot.z:.3f}, {-(rot.w):.3f}, -1")
+    else:
+        string = obj.name        
+    
+    print(f"  0000, {string}, 0, {coord.x:.3f}, {coord.y:.3f}, {coord.z:.3f}, {rot.x:.3f}, {rot.y:.3f}, {rot.z:.3f}, {-(rot.w):.3f}, -1")
 
 print("\n\n")
