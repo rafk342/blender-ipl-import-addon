46ae8863b030b1fd28bc979eb8b41c485d573d74
swapfr <97851724+swapfr@users.noreply.github.com>
2023-04-11 19:10:25 +0300

    Update ipl_format_line_in_console.py

--- ipl_format_line_in_console.py
+++ ipl_format_line_in_console.py
@@ -18,9 +18,9 @@ for obj in selected_objects:
     rot_x = 0 if rot.x == 0.0 else f"{rot.x:.3f}"
     rot_y = 0 if rot.y == 0.0 else f"{rot.y:.3f}"
     rot_z = 0 if rot.z == 0.0 else f"{rot.z:.3f}"
-    rot_w = 0 if rot.w == 0.0 else f"{rot.w:.3f}"
+    rot_w = 0 if rot.w == 0.0 else f"{-rot.w:.3f}"
         
         
-    print(f"0000, {string}, 0, {coord.x:.3f}, {coord.y:.3f}, {coord.z:.3f}, {rot_x}, {rot_y}, {rot_z}, {-float(rot_w)}, -1")
+    print(f"0000, {string}, 0, {coord.x:.3f}, {coord.y:.3f}, {coord.z:.3f}, {rot_x}, {rot_y}, {rot_z}, {rot_w}, -1")
 
 print("\n\n")
