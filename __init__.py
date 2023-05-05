af716407364b64311cf0fa82b935d188db1b1e21
swapfr <97851724+swapfr@users.noreply.github.com>
2023-05-05 18:10:30 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -37,13 +37,15 @@ classes_array = [
     import_ipl_file, 
     print_selected_objects_operator, 
     ipl_panel, 
-    set_object_id
+    set_object_id,
+    show_console
     ]   
 
 def register():
     for _class in classes_array:
         bpy.utils.register_class(_class)
 
+    print(" \n\n register ipl \n\n ")
 
     bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
     bpy.types.Scene.object_id = bpy.props.IntProperty(name="Object ID", default= 0000)
@@ -52,6 +54,7 @@ def unregister():
     for _class in classes_array:
         bpy.utils.unregister_class(_class)
     
+    print(" \n\n unregister ipl \n\n ")
     
     bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
     del bpy.types.Scene.object_id
